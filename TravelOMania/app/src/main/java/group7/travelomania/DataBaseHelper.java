cc8669d8c58f3b64963d673a0d9d455cfad2a3a3
Matt <mkschleg@umail.iu.edu>
2015-10-23 15:35:20 -0400

    Adding Databasehelper class.

---
+++ TravelOMania/app/src/main/java/group7/travelomania/DataBaseHelper.java
@@ -0,0 +1,125 @@
+package group7.travelomania;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+
+/**
+ * Created by Matt on 10/23/15.
+ * DataBaseHelper class
+ * Used to open, and create our database.
+ */
+public class DataBaseHelper extends SQLiteOpenHelper{
+
+    //TODO add the database path!
+
+    private String DB_PATH;
+
+    private static String DB_NAME = "travel_db";
+
+    private SQLiteDatabase database;
+
+    private final Context context;
+
+
+    public DataBaseHelper(Context con){
+        super(con, DB_NAME, null, 1);
+        this.context = con;
+        DB_PATH = context.getDatabasePath(DB_NAME).toString();
+
+    }
+
+    public void createDataBase() throws IOException{
+        boolean dbExist = checkDataBase();
+        if(!dbExist){
+            this.getReadableDatabase();
+            try{
+                copyDataBase();
+            }
+            catch (IOException e){
+                throw new Error("Error Copying Database");
+            }
+        }
+    }
+
+
+
+    public boolean checkDataBase(){
+
+        SQLiteDatabase checkDB = null;
+
+        try{
+            String path = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
+        }
+        catch (SQLiteException e){
+            //Does not exist;
+            Log.v("Database Check", "Need to create the database.");
+        }
+
+        if(checkDB != null){
+             checkDB.close();
+        }
+
+        return checkDB!=null;
+    }
+
+    public void copyDataBase() throws IOException{
+        InputStream input = context.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream output = new FileOutputStream(outFileName);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while((length = input.read(buffer)) > 0){
+            output.write(buffer,0,length);
+        }
+
+        output.flush();
+        output.close();
+        input.close();
+
+    }
+
+    public void openDataBase() throws SQLException{
+
+        String path = DB_PATH + DB_NAME;
+        database = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY);
+
+    }
+
+    @Override
+    public synchronized void close(){
+
+        if(database != null){
+            database.close();
+        }
+
+        super.close();
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+
+
+
+
+
+}
