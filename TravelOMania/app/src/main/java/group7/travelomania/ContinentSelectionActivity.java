caddbaf267c2dc4135c2c64624c02d437171ae0e
Matt <mkschleg@umail.iu.edu>
2015-10-26 00:28:17 -0400

    Started on TODO in continentSelectionActivity. Needs more research before completion.

--- TravelOMania/app/src/main/java/group7/travelomania/ContinentSelectionActivity.java
+++ TravelOMania/app/src/main/java/group7/travelomania/ContinentSelectionActivity.java
@@ -1,10 +1,14 @@
 package group7.travelomania;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.graphics.Bitmap;
 
 public class ContinentSelectionActivity extends AppCompatActivity {
 
@@ -12,7 +16,10 @@ public class ContinentSelectionActivity extends AppCompatActivity {
     private ImageView map;
     private ImageView avatar;
 
+    private Bitmap bitmapMap;
 
+    private int mapHeight, mapWidth;
+    private float mapX, mapY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,29 @@ public class ContinentSelectionActivity extends AppCompatActivity {
 
         map = (ImageView) findViewById(R.id.imageView_map);
         avatar = (ImageView) findViewById(R.id.imageView_avatar);
+
+
+        map.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //Remove it here unless you want to get this callback for EVERY
+                //layout pass, which can get you into infinite loops if you ever
+                //modify the layout from within this method.
+                if (Build.VERSION.SDK_INT >= 16)
+                    map.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                else
+                    map.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                mapHeight = (int) Math.floor(map.getWidth() * 0.61087511d);
+                mapWidth = map.getWidth();
+                mapX = map.getX();
+                mapY = map.getY() + (int) Math.ceil((map.getHeight() - mapHeight) / 2);
+                Log.v("Map W, H, X, Y", Integer.toString(mapWidth) + " " +
+                        Integer.toString(mapHeight) + " " +
+                        Float.toString(mapX) + " " +
+                        Float.toString(mapY));
+                //Now you can get the width and height from content
+            }
+        });
 
         map.setOnTouchListener(new View.OnTouchListener() {
             @Override
