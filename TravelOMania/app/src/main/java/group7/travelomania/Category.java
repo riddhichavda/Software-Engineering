9adae4425e8e2ee81bffcf6efaf027df97f6616a
hermione <liyuy@indiana.edu>
2015-10-29 18:39:17 -0400

    Category go to next activity

---
+++ TravelOMania/app/src/main/java/group7/travelomania/Category.java
@@ -0,0 +1,13 @@
+package group7.travelomania;
+
+/**
+ * Created by Grace Li on 10/29/15.
+ */
+public enum Category {
+    Politics,
+    Sports,
+    Landmarks,
+    Cuisines,
+    Fesitvals,
+    Capitals
+}
--- TravelOMania/app/src/main/java/group7/travelomania/CategorySelectionActivity.java
+++ TravelOMania/app/src/main/java/group7/travelomania/CategorySelectionActivity.java
@@ -1,16 +1,52 @@
 package group7.travelomania;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
-public class CategorySelectionActivity extends AppCompatActivity {
+public class CategorySelectionActivity extends AppCompatActivity
+{
 
+    Category categorySelected;
+    Category currentCategory;
+
+    private ImageView category;
+    private Bitmap bitmap;
+    private boolean isNewSelection;
+    private int caHight, caWidth;
+    Player player;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_selection);
-    }
+
+        categorySelected = null;
+
+        category = (ImageView) findViewById(R.id.imageView_category);
+        final Button buttonNext = (Button) findViewById(R.id.buttonNext);
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToNextActivity();
+
+            }
+        });
+
 }
+    private void goToNextActivity(){
+        Intent intent = new Intent(this, ScoreCardActivity.class);
+        startActivity(intent);
+
+
+    }
+        }
+
+
