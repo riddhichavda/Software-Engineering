de0561f14cc2543c3b2e08f0e4c4f8f1ff564f3a
Matt <mkschleg@umail.iu.edu>
2015-11-03 15:00:59 -0500

    Added Riddhi's Scorecard!

--- TravelOMania/app/src/main/java/group7/travelomania/ScoreCardActivity.java
+++ TravelOMania/app/src/main/java/group7/travelomania/ScoreCardActivity.java
@@ -1,15 +1,22 @@
 package group7.travelomania;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ScoreCardActivity extends AppCompatActivity {
 
 
     private Player player;
+    private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,53 @@ public class ScoreCardActivity extends AppCompatActivity {
         player = Player.getInstance(this);
 
         final Button btn_nextLevel = (Button) findViewById(R.id.next_level);
+        final Button btn_restartLevel = (Button) findViewById(R.id.restart_level);
+
+        //Initialize views.
+
+
+        avatar = (ImageView) findViewById(R.id.imageView_avatar);
+        avatar.setImageBitmap(player.avatar);
+
+
+        int total_score = player.totalScore;
+        if(total_score>=50 && total_score<=70)
+        {
+            TextView txtScore = (TextView)findViewById(R.id.score_value);
+            txtScore.setText(Integer.toString(total_score));
+            TextView txtHint = (TextView)findViewById(R.id.number_of_hints);
+            txtHint.setText("+1");
+            ImageView img = (ImageView)findViewById(R.id.imageView);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                img.setBackground(ContextCompat.getDrawable(this, R.drawable.excellent));
+            }
+            img.setVisibility(View.VISIBLE);
+            //load an imageview bulb
+
+
+        }
+        else if(total_score<50 && total_score>20)
+        {
+            TextView txtScore = (TextView)findViewById(R.id.score_value);
+            txtScore.setText(Integer.toString(total_score));
+            TextView txtHint = (TextView)findViewById(R.id.number_of_hints);
+            txtHint.setText("0");
+            ImageView img = (ImageView)findViewById(R.id.imageView);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                img.setBackground(ContextCompat.getDrawable(this, R.drawable.good_job));
+            }
+            img.setVisibility(View.VISIBLE);
+
+            //load an imageview bulb
+
+        }
+        else if(total_score >= 0 && total_score <= 20) {
+
+            btn_nextLevel.setVisibility(Button.INVISIBLE);
+        }
+        else {
+            System.out.println("---------------------invalid total score-----------------");
+        }
 
         btn_nextLevel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +80,19 @@ public class ScoreCardActivity extends AppCompatActivity {
                 nextLevel();
             }
         });
+        btn_restartLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
 
+                restartLevel();
+            }
+        });
     }
-
+    private void restartLevel(){
+        Intent intent;
+        intent = new Intent(this,LevelActivity.class);
+        startActivity(intent);
+    }
     private void nextLevel(){
         player.continentsTraveled.add(0,player.currentContinent);
         player.currentContinent = null;
